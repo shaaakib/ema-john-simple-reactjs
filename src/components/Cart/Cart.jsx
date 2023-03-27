e38322ef54944961e003541c562869a8ea7d8e22
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-03-28 05:31:29 +0600

    save cart to local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -19,9 +19,9 @@ export default function Cart({ cart }) {
       <h4 className="text-lg font-bold text-center my-5">Order Summary</h4>
       <p className="py-2 text-xl">Selected Item: {cart.length}</p>
       <p className=" text-xl">Total Price: ${totalPrice}</p>
-      <p className="py-2 text-xl">Shipping: {totalShipping}</p>
-      <p className=" text-xl">Tax: {tax.toFixed(2)}</p>
-      <h6 className="py-2 text-xl">Grand Total: {grandTotal.toFixed(2)}</h6>
+      <p className="py-2 text-xl">Shipping: ${totalShipping}</p>
+      <p className=" text-xl">Tax: ${tax.toFixed(2)}</p>
+      <h6 className="py-2 text-xl">Grand Total: ${grandTotal.toFixed(2)}</h6>
     </div>
   );
 }
